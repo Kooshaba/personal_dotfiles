4da5240386e13433ddd3841e3a65017d7fb6479e
Kooshaba <kooshaba@gmail.com>
2015-07-24 23:55:13 -0700

    get rid of on

--- main.sh
+++ main.sh
@@ -16,6 +16,6 @@ if [ $SSH_TTY ]; then
   PS1="$RED\u@\h $RED\w $LIGHTRED\$(current_git_branch)\$(git_special)$YELLOW\$(local_user)$LIGHTRED$ $NC"
   PS1="$PS1\[\e]0;\u@\h\a\]"
 else
-  PS1="$GREEN\u@\h $RED\w$NC on $BROWN\$(current_git_branch)\$(git_special)$BROWN\n[\@]$YELLOW\$(local_user)$GREEN$ $NC"
+  PS1="$GREEN\u@\h $RED\w$NC $BROWN\$(current_git_branch)\$(git_special)$BROWN\n[\@]$YELLOW\$(local_user)$GREEN$ $NC"
   PS1="$PS1\[\e]0;\a\]"
 fi
