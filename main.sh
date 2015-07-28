12f42f77534423dc8ad60c2b8fa931a50c9742a7
Andy Cernera <andy.cernera@omadahealth.com>
2015-07-28 14:47:05 -0700

    history alias and history number on line

--- main.sh
+++ main.sh
@@ -11,6 +11,8 @@ alias be="bundle exec"
 alias bashconfig="$EDITOR ~/.bashrc && reload"
 alias cpkey="pbcopy < ~/.ssh/id_rsa.pub"
 alias reload=". ~/.bash_profile && echo 'bash config reloaded from ~/.bash_profile'"
+alias h="history 40"
+
 
 alias matrix='LC_ALL=C tr -c "[:digit:]" " " < /dev/urandom | dd cbs=$COLUMNS conv=unblock | GREP_COLOR="1;32" grep --color "[^ ]"'
 
@@ -18,6 +20,6 @@ if [ $SSH_TTY ]; then
   PS1="$RED\u@\h $RED\w $LIGHTRED\$(current_git_branch)\$(git_special)$YELLOW\$(local_user)$LIGHTRED$ $NC"
   PS1="$PS1\[\e]0;\u@\h\a\]"
 else
-  PS1="$GREEN\u@\h $RED\w$NC $BROWN\$(current_git_branch)\$(git_special)$BROWN\n[\@]$YELLOW\$(local_user)$GREEN$ $NC"
+  PS1="$GREEN\u@\h $RED\w$NC $BROWN\$(current_git_branch)\$(git_special)$BROWN\n[\@]$RED \!$YELLOW\$(local_user)$GREEN$ $NC"
   PS1="$PS1\[\e]0;\a\]"
 fi
