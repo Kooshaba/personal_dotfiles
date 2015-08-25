557ee147f98a9c9c6fa56892cfd461ec8c29096e
Andy Cernera <kooshaba@gmail.com>
2015-08-25 14:50:33 -0700

    add psgrep and killsophos functions

--- main.sh
+++ main.sh
@@ -7,11 +7,16 @@ else
 fi
 
 alias gitconfig="git config --edit"
+alias gca="git commit --amend --reset-author"
+
 alias be="bundle exec"
-alias bashconfig="$EDITOR ~/.bashrc && reload"
+alias bashconfig="$EDITOR ~/personal_dotfiles/ && reload"
 alias cpkey="pbcopy < ~/.ssh/id_rsa.pub"
 alias reload=". ~/.bash_profile && echo 'bash config reloaded from ~/.bash_profile'"
 alias h="history 40"
+
+psgrep() { ps aux | grep $1 | grep -v grep; }
+killsophos() { psgrep sophos | awk '{print $2}' | xargs sudo kill -9; }
 
 
 alias matrix='LC_ALL=C tr -c "[:digit:]" " " < /dev/urandom | dd cbs=$COLUMNS conv=unblock | GREP_COLOR="1;32" grep --color "[^ ]"'
@@ -23,6 +28,3 @@ else
   PS1="$GREEN\u@\h $RED\w$NC $BROWN\$(current_git_branch)\$(git_special)$BROWN\n[\@]$RED\!$YELLOW\$(local_user)$GREEN$ $NC"
   PS1="$PS1\[\e]0;\a\]"
 fi
-
-history -c
-history -w
