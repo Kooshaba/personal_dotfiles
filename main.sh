b48809e9aadd3f6e2a02be591dc9864cd6467100
Andy Cernera <kooshaba@gmail.com>
2019-09-05 10:56:56 -0700

    switch to zsh and remove dependency on omada dotfiles

--- main.sh
+++ main.sh
@@ -11,7 +11,11 @@ alias gc="git commit"
 alias gca="gc --amend --reset-author"
 alias gcan="gca --no-edit"
 alias gpr="git pull --rebase"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias glh="gl | head"
+alias gst="git status"
+alias gv="git pull --rebase"
+alias 'g^'="git push -u origin HEAD"
 
 alias dc="docker-compose"
 alias d="docker"
@@ -21,14 +25,13 @@ dnuke() { docker rm $(docker ps -a -q); }
 dnukei() { docker rmi $(docker images | grep 'none>' | awk '{print $3}'); }
 dbash() { docker run -it $1 /bin/bash; }
 
-alias hc="hyper compose"
-alias hcrun="hyper compose run -f hyper-compose.yml --rm"
-hnuke() { hyper rm $(hyper ps -a -q);  }
+alias etm="ember t --module"
+alias etf="ember t -f"
 
 alias be="bundle exec"
-alias bashconfig="$EDITOR ~/workspace/personal_dotfiles/main.sh && reload"
+# alias bashconfig="$EDITOR ~/workspace/personal_dotfiles/main.sh && reload"
 alias cpkey="pbcopy < ~/.ssh/id_rsa.pub"
-alias reload=". ~/.bash_profile && echo 'bash config reloaded from ~/.bash_profile'"
+alias reload=". ~/.zshrc && echo 'reloaded .zshrc'"
 alias h="history 40"
 
 alias p="psql"
@@ -40,21 +43,11 @@ alias date_folder="mkdir `pdate`"
 
 alias work="cd ~/workspace"
 
-alias ssh="~/workspace/personal_dotfiles/ssh-host-color"
+# alias ssh="~/workspace/personal_dotfiles/ssh-host-color"
 alias grep="grep --color=auto"
 
 psgrep() { ps aux | grep $1 | grep -v grep; }
 pskill() { psgrep $1 | awk '{print $2}' | xargs sudo kill -9; }
 watch() { while :; do clear; $1; sleep 2; done }
 
-alias hl='hyperlayout'
-
 alias matrix='LC_ALL=C tr -c "[:digit:]" " " < /dev/urandom | dd cbs=$COLUMNS conv=unblock | GREP_COLOR="1;32" grep --color "[^ ]"'
-
-if [ $SSH_TTY ]; then
-  PS1="$RED\u@\h $RED\w $LIGHTRED$ $NC"
-  PS1="$PS1\[\e]0;\u@\h\a\]"
-else
-  PS1="$GREEN\u@\h $RED\w$NC $BROWN\$(current_git_branch)\$(git_special)$BROWN\n[\@]$RED\!$YELLOW\$(local_user)${GREEN}➤➤➤ $NC"
-	PS1="$PS1\[\e]0;\a\]"
-fi
